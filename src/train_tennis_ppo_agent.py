301f3cbcc859d04ee972979d5d9d8c6c0d4a0cca
Daniel Sotingco <daniel.sotingco@gmail.com>
2020-07-20 13:45:36 -0400

    chore: Add new file to hold upcoming Actor-Critic implementation.

--- src/train_tennis_ppo_agent.py
+++ src/train_tennis_ppo_agent.py
@@ -7,14 +7,14 @@ import numpy as np
 import torch
 import torch.optim as optim
 import tennis_ppo_utils
-import TennisPpoPolicy
+import TennisActorCritic
 import ReplayBuffer
 import matplotlib.pyplot as plt
 from collections import deque
 
 # Hyperparameters
 learning_rate = 2e-4
-num_episodes = 10000
+num_episodes = 10
 discount = 0.999
 epsilon = 0.15
 beta = 0.02
@@ -29,10 +29,10 @@ use_high_score_replay_buffer = False
 # Environment setup
 env = UnityEnvironment(file_name="Tennis.exe")
 num_agents = 2
-policy = TennisPpoPolicy.TennisPpoPolicy()
+agent = TennisActorCritic.TennisActorCritic()
 replay_buffer = ReplayBuffer.ReplayBuffer(batch_size=batch_size)
 high_score_replay_buffer = ReplayBuffer.ReplayBuffer(batch_size=batch_size)
-optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
+optimizer = optim.Adam(agent.parameters(), lr=learning_rate)
 
 # Initialize scores, etc.
 high_score = 0.0
@@ -44,7 +44,7 @@ scores_window = deque(maxlen=100)
 # Run episodes and train agent.
 for episode in range(num_episodes):
     # Collect trajectories
-    (prob_list, state_list, action_list, reward_list, episode_score) = tennis_ppo_utils.collect_trajectories(env, policy)
+    (prob_list, state_list, action_list, reward_list, episode_score) = tennis_ppo_utils.collect_trajectories(env, agent)
     processed_rewards = tennis_ppo_utils.process_rewards(reward_list)
     if(episode_score > replay_buffer_store_threshold):
         replay_buffer.add_episode(prob_list, state_list, action_list, processed_rewards.tolist())
@@ -70,13 +70,13 @@ for episode in range(num_episodes):
         # print("training...")
         # Run training epochs
         for epoch in range(num_epochs_per_episode):
-            tennis_ppo_utils.run_training_epoch(policy, optimizer, replay_buffer,
+            tennis_ppo_utils.run_training_epoch(agent, optimizer, replay_buffer,
                                             discount=discount, epsilon=epsilon, beta=beta, batch_size=batch_size)
             replay_buffer.clear()
         if use_high_score_replay_buffer:
             if high_score_replay_buffer.has_enough_samples():
                 for epoch in range(num_epochs_per_episode):
-                    tennis_ppo_utils.run_training_epoch(policy, optimizer, high_score_replay_buffer,
+                    tennis_ppo_utils.run_training_epoch(agent, optimizer, high_score_replay_buffer,
                                                     discount=discount, epsilon=epsilon, beta=beta, batch_size=batch_size)
                 if high_score_replay_buffer.sampled_count > high_score_buffer_sample_limit:
                     print("Clearing high score replay buffer")
