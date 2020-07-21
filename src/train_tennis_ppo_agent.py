75182d1a7ef7e2803985190d886b9ab25430dd8a
Daniel Sotingco <daniel.sotingco@gmail.com>
2020-07-21 13:37:52 -0400

    chore: Remove unused code for actor-critic.

--- src/train_tennis_ppo_agent.py
+++ src/train_tennis_ppo_agent.py
@@ -13,25 +13,21 @@ import matplotlib.pyplot as plt
 from collections import deque
 
 # Hyperparameters
-learning_rate = 1e-4
+learning_rate = 2e-4
 num_episodes = 1000
 discount = 0.995
-epsilon = 0.15
+epsilon = 0.10
 beta = 0.02
 batch_size = 128
-replay_buffer_store_threshold = -1.0
-high_score_buffer_sample_limit = 100
 num_epochs_per_episode = 10
 score_save_threshold = 0.25
 score_solved_threshold = 0.60
-use_high_score_replay_buffer = False
 
 # Environment setup
 env = UnityEnvironment(file_name="Tennis.exe")
 num_agents = 2
 agent = TennisActorCritic.TennisActorCritic()
-replay_buffer = TrajectoryBuffer.TrajectoryBuffer(batch_size=batch_size, num_batches=num_epochs_per_episode)
-high_score_replay_buffer = TrajectoryBuffer.TrajectoryBuffer(batch_size=batch_size, num_batches=num_epochs_per_episode)
+trajectory_buffer = TrajectoryBuffer.TrajectoryBuffer(batch_size=batch_size, num_batches=num_epochs_per_episode)
 optimizer = optim.Adam(agent.parameters(), lr=learning_rate)
 
 # Initialize scores, etc.
@@ -45,14 +41,10 @@ scores_window = deque(maxlen=100)
 for episode in range(num_episodes):
     # Collect trajectories
     (prob_list, state_list, action_list, reward_list, state_value_list, episode_score) = tennis_ppo_utils.collect_trajectories(env, agent)
-    # actor_critic_advantage = tennis_ppo_utils.calculate_normalized_advantage(reward_list, state_value_list)
     discounted_future_rewards = tennis_ppo_utils.calculate_discounted_future_rewards(reward_list, discount)
-    if(episode_score > replay_buffer_store_threshold):
-        replay_buffer.add_episode(prob_list, state_list, action_list, discounted_future_rewards.tolist(), state_value_list)
-    if(episode_score >= high_score):
+    if(episode_score > high_score):
         high_score = episode_score
-        print("high score:", high_score)
-        high_score_replay_buffer.add_episode(prob_list, state_list, action_list, discounted_future_rewards.tolist(), state_value_list)
+        print("new high score:", high_score)
 
     # Process scores
     episode_scores.append(episode_score)
@@ -67,21 +59,13 @@ for episode in range(num_episodes):
         print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode-100, np.mean(scores_window)))
         break
 
-    if replay_buffer.has_enough_samples():
+    if trajectory_buffer.has_enough_samples():
         # print("training...")
         # Run training epochs
         for epoch in range(num_epochs_per_episode):
-            tennis_ppo_utils.run_training_epoch(agent, optimizer, replay_buffer,
+            tennis_ppo_utils.run_training_epoch(agent, optimizer, trajectory_buffer,
                                             discount=discount, epsilon=epsilon, beta=beta, batch_size=batch_size)
-            replay_buffer.clear()
-        if use_high_score_replay_buffer:
-            if high_score_replay_buffer.has_enough_samples():
-                for epoch in range(num_epochs_per_episode):
-                    tennis_ppo_utils.run_training_epoch(agent, optimizer, high_score_replay_buffer,
-                                                    discount=discount, epsilon=epsilon, beta=beta, batch_size=batch_size)
-                if high_score_replay_buffer.sampled_count > high_score_buffer_sample_limit:
-                    print("Clearing high score replay buffer")
-                    high_score_replay_buffer.clear()
+            trajectory_buffer.clear()
 
         # the clipping parameter reduces as time goes on
         epsilon*=.999
