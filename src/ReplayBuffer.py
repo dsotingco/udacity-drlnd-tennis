dbf30482d6e7c811d937cc454d00911ebb868376
Daniel Sotingco <daniel.sotingco@gmail.com>
2020-07-21 09:21:30 -0400

    bugfix: Fixed batch sampling to use all data, and also wait until there are enough samples to fill all epochs.

--- src/ReplayBuffer.py
+++ src/ReplayBuffer.py
@@ -6,17 +6,19 @@ from collections import deque
 
 class ReplayBuffer:
 
-    def __init__(self, buffer_size=1024, batch_size=128):
+    def __init__(self, buffer_size=1280, batch_size=128, num_batches=10):
         self.prob_memory = deque(maxlen=buffer_size)
         self.state_memory = deque(maxlen=buffer_size)
         self.action_memory = deque(maxlen=buffer_size)
         self.reward_memory = deque(maxlen=buffer_size) # for discounted_future_rewards
         self.state_value_memory = deque(maxlen=buffer_size)
         self.batch_size = batch_size
+        self.num_batches = num_batches
         self.sampled_count = 0
+        self.sample_indices = np.arange(buffer_size)
 
     def has_enough_samples(self):
-        return ( len(self.state_memory) >= self.batch_size )
+        return ( len(self.state_memory) >= ( self.batch_size * self.num_batches ) )
 
     def clear(self):
         self.prob_memory.clear()
@@ -24,6 +26,7 @@ class ReplayBuffer:
         self.action_memory.clear()
         self.reward_memory.clear()
         self.state_value_memory.clear()
+        self.sampled_count = 0
 
     def add_episode(self, prob_list, state_list, action_list, processed_reward_list, state_value_list):
         self.prob_memory.extend(prob_list)
@@ -34,10 +37,11 @@ class ReplayBuffer:
 
     def sample(self):
         assert self.has_enough_samples()
+        if self.sampled_count == 0:
+            np.random.shuffle(self.sample_indices)
+        sample_start_index = self.sampled_count * self.batch_size
+        sample_end_index = sample_start_index + self.batch_size
         self.sampled_count += 1
-        num_samples = len(self.state_memory)
-        sample_indices = np.arange(num_samples)
-        np.random.shuffle(sample_indices)
 
         old_prob_tensor = torch.stack(list(self.prob_memory)).detach()
         old_prob_tensor_summed = torch.sum(old_prob_tensor, axis=2)
@@ -46,11 +50,11 @@ class ReplayBuffer:
         reward_tensor = torch.tensor(list(self.reward_memory), dtype=torch.float)
         state_value_tensor = torch.transpose(torch.cat(list(self.state_value_memory), axis=1), 0, 1).detach()
 
-        old_prob_batch = old_prob_tensor_summed[0 : self.batch_size]
-        state_batch = state_tensor[0 : self.batch_size]
-        action_batch = action_tensor[0 : self.batch_size]
-        reward_batch = reward_tensor[0 : self.batch_size]
-        state_value_batch = state_value_tensor[0 : self.batch_size]
+        old_prob_batch = old_prob_tensor_summed[sample_start_index : sample_end_index]
+        state_batch = state_tensor[sample_start_index : sample_end_index]
+        action_batch = action_tensor[sample_start_index : sample_end_index]
+        reward_batch = reward_tensor[sample_start_index : sample_end_index]
+        state_value_batch = state_value_tensor[sample_start_index : sample_end_index]
 
         assert(old_prob_batch.shape == torch.Size([self.batch_size,2]))
         assert(state_batch.shape == torch.Size([self.batch_size,2,24]))
--- src/train_tennis_ppo_agent.py
+++ src/train_tennis_ppo_agent.py
@@ -30,8 +30,8 @@ use_high_score_replay_buffer = False
 env = UnityEnvironment(file_name="Tennis.exe")
 num_agents = 2
 agent = TennisActorCritic.TennisActorCritic()
-replay_buffer = ReplayBuffer.ReplayBuffer(batch_size=batch_size)
-high_score_replay_buffer = ReplayBuffer.ReplayBuffer(batch_size=batch_size)
+replay_buffer = ReplayBuffer.ReplayBuffer(batch_size=batch_size, num_batches=num_epochs_per_episode)
+high_score_replay_buffer = ReplayBuffer.ReplayBuffer(batch_size=batch_size, num_batches=num_epochs_per_episode)
 optimizer = optim.Adam(agent.parameters(), lr=learning_rate)
 
 # Initialize scores, etc.
