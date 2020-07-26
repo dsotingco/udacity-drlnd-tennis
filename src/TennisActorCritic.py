bfe5d67d5de69d7389d9376486519cc039cf43e3
Daniel Sotingco <daniel.sotingco@gmail.com>
2020-07-26 14:50:24 -0400

    feat: Reduced neural network hidden layers to 64x64 to match PPO paper.

--- src/TennisActorCritic.py
+++ src/TennisActorCritic.py
@@ -8,22 +8,21 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class TennisActorCritic(nn.Module):
     """ Policy model. """
 
-    def __init__(self, state_size=24, hidden1_size=128, hidden2_size=64, hidden3_size=32, action_size=2, 
+    def __init__(self, state_size=24, hidden1_size=64, hidden2_size=64, action_size=2, 
                  init_std_deviation=1.0):
         super(TennisActorCritic, self).__init__()
 
         # Shared layers to understand the environment
         self.shared_fc1 = nn.Linear(state_size, hidden1_size)
         self.shared_fc2 = nn.Linear(hidden1_size, hidden2_size)
-        self.shared_fc3 = nn.Linear(hidden2_size, hidden3_size)
 
         # Actor layer(s), parameters
-        self.actor_fc1 = nn.Linear(hidden3_size, action_size)
+        self.actor_fc1 = nn.Linear(hidden2_size, action_size)
         # Output of Actor neural network: [mu1; mu2]
         self.std_deviations = nn.Parameter(init_std_deviation * torch.ones(1, action_size))
 
         # Critic layers
-        self.critic_fc1 = nn.Linear(hidden3_size, 1)
+        self.critic_fc1 = nn.Linear(hidden2_size, 1)
         # Output of Critic neural network: scalar estimate of state value 
 
     def forward(self, state, actions=None, training_mode=True):
@@ -37,7 +36,6 @@ class TennisActorCritic(nn.Module):
         # Shared layers
         x = F.relu(self.shared_fc1(state))
         x = F.relu(self.shared_fc2(x))
-        x = F.relu(self.shared_fc3(x))
 
         # ACTOR
         means = torch.tanh(self.actor_fc1(x))
